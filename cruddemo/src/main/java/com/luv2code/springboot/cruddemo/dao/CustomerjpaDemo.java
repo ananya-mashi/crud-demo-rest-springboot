51315d01879c78b027db660eda81b31e7bffcba2
Ananya <60173002+ananya-mashi@users.noreply.github.com>
2020-04-29 15:03:03 +0530

    Add files via upload

---
+++ cruddemo/src/main/java/com/luv2code/springboot/cruddemo/dao/CustomerjpaDemo.java
@@ -0,0 +1,33 @@
+package com.luv2code.springboot.cruddemo.dao;
+
+import java.util.List;
+
+import com.luv2code.springboot.cruddemo.entity.Customer;
+
+public class CustomerjpaDemo implements CustomerDAO {
+
+	@Override
+	public List<Customer> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Customer findById(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void deleteById(int id) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void save(Customer customer) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
